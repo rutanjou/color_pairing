38df036c181625b75cfb43f85950901d2d9fd68b
Ru Tan <jutan2012@gmail.com>
2016-10-07 14:00:36 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-// good luck !
+// good luck
+
+    
+
+    $("input").css("background", function(){
+           return $(this).data("color");
+    });
+
+    $("input").on("click",function(){
+         var colors = $(this).data("color");
+         var checkbox = $("[type='checkbox']");
+    // $("body").css("background",colors);
+      
+       var checked = checkbox.is(':checked');
+       console.log(checked);
+       if (checked){
+       	$(".main").css('color',colors);
+       }else{
+       	$("body").css('background',colors);
+       };
+
+});
+
+/* Si je click alors{
+	si (cochér){
+	je change le texte
+	}si non (je change le fond);
+
+
+
+}*/
+
+
+
+    
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
